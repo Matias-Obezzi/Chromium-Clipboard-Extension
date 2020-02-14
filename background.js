06bf70be9c8718a07a7fd45b7146718c82e9ffed
Matias-Obezzi <matiasobezzi@gmail.com>
2020-02-13 22:26:55 -0300

    Notifications and options menu

--- background.js
+++ background.js
@@ -1,45 +1,121 @@
-var content;
+var content = [];
+var lang = "es";
+var notif = false;
+var contBase = 0;
+var cont = 0;
+var alarm;
+var opt = {
+    type: "basic",
+    title: "Clipboard",
+    message: "",
+    iconUrl: "media/icon.png"
+}
+
+load();
 
 chrome.commands.onCommand.addListener(function(command){
-    chrome.storage.local.get(['clipboard'], function(clip){
-        if(undefined === clip.clipboard || "" === clip.clipboard){
-            content=[];
-        }else{
-            content = clip.clipboard;
-        }
-    });
     if(command==="copy"){
         chrome.tabs.executeScript({
             code:"window.getSelection().getRangeAt(0).toString();"
         }, function(selection){
-            if(undefined !== selection){
+            if(undefined !== selection && selection.toString()!==""){
                 content.push(generate(selection));
                 chrome.storage.local.set({'clipboard': content}, function(data){
-                    chrome.tabs.executeScript({
-                        code:"console.log(\""+content + "\");"
-                    });
                 });
-                
+                cont+=1;
+                if(notif){
+                    if(lang==="es"){
+                        textNotification = "Añadido";
+                    }else{
+                        textNotification = "Added";
+                    }
+                }else{
+                    textNotification = cont-contBase;
+                }
+                notification(textNotification);
             }
         });
     }else if(command === "clear"){
         chrome.storage.local.set({'clipboard': ""}, function(){
             content = [];
+            cont = 0;
+            contBase = 0;
+            if(notif){
+                if(lang==="es"){
+                    textNotification = "Borrado";
+                }else{
+                    textNotification = "Cleared";
+                }
+            }else{
+                textNotification = 0;
+            }
+            notification(textNotification);
         });
+        
     }
 });
 
-function generate(text){
-    text = text.toString().replace(/\"|\'|\n/gi, function(data){
-        if(data === '\"'){
-            return '\\"';
-        }else if(data === "\'"){
-            return "\\'";
+chrome.storage.local.onChanged.addListener(function(){
+    load();
+    message("change");
+});
+
+function load(){
+    loadClip();
+    loadLang();
+    loadNotif();
+}
+
+function loadClip(){
+    chrome.storage.local.get(['clipboard'], function(clip){
+        if(undefined === clip.clipboard || "" === clip.clipboard){
+            content=[];
         }else{
-            return " ";
+            content = clip.clipboard;
+            contBase = content.length();
+            cont = contBase;
         }
     });
+}
 
+function loadLang(){
+    chrome.storage.local.get('lang', function(data){
+        if(undefined === data.lang){
+            chrome.storage.local.set({'lang': lang}, function(data){
+                lang = "es";
+            });
+        }else{
+            lang = data.lang;
+        }
+    });
+}
+
+function loadNotif(){
+    chrome.storage.local.get('notif', function(data){
+        if(undefined === data.notif){
+            chrome.storage.local.set({'notif': notif}, function(data){
+                notif = false;
+            });
+        }else{
+            notif = data.notif;
+        }
+    });
+}
+
+function notification(textNotification){
+    if(notif){
+        opt.message=textNotification;
+        chrome.notifications.create(opt);
+    }else{
+        chrome.browserAction.setBadgeText({"text": textNotification.toString() + ""});
+        setTimeout(()=>{
+            chrome.browserAction.setBadgeText({"text": ""});
+        }, 3000);
+    }
+}
+
+function generate(text){
+    text = text.toString().replace(/\"|\'|\n/gi, "");
     var m = new Date();
     var time =
         ("0" + m.getDate()).slice(-2) + "/" +
--- scripts/popup.js
+++ scripts/popup.js
@@ -2,10 +2,27 @@ $("#content").css("margin-top", $("#menu").height() + 10);
 
 var content = [];
 var lang = "es";
+var notif = false;
+var touch = false;
 var tooltip;
 var button;
 var popperInstance = null;
+
+chrome.commands.onCommand.addListener(function(command){
+    if(command === "clear"){
+        chrome.storage.local.set({'clipboard': ""}, function(){
+            content = [];
+        });
+        $("#content").html("");
+    }
+});
+
 main();
+
+function generateCard(text, timeBase){
+    time = dateString(timeBase);
+    return '<div class="card mx-auto m-2 my-4 p-2 text-center w-100 blockquote mb-0"><div class="d-flex justify-content-end"><a id="copy" href="#" class="icon-clipboard"></a><div id="tooltip" role="tooltip">Copy<div id="arrow" data-popper-arrow></div></div></div><p>' + text + '</p><footer class="blockquote-footer text-center w-75 mx-auto">' + time + '</footer></div>';
+}
 
 function dateString(timeBase){
     dateTemp = timeBase.split(" ")[0].split("/");
@@ -48,11 +65,6 @@ function dateString(timeBase){
     }
 }
 
-function generateCard(text, timeBase){
-    time = dateString(timeBase);
-    return '<div class="card mx-auto m-2 my-4 p-2 text-center w-100 blockquote mb-0"><div class="d-flex justify-content-end"><a id="copy" href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,9H7V7H17M17,13H7V11H17M14,17H7V15H14M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3M19,3H14.82C14.4,1.84 13.3,1 12,1C10.7,1 9.6,1.84 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3Z" /></svg></a><div id="tooltip" role="tooltip">Copy<div id="arrow" data-popper-arrow></div></div></div><p>' + text + '</p><footer class="blockquote-footer text-center w-75 mx-auto">' + time + '</footer></div>';
-}
-
 function copyToClipboard(text, el) {
     var copyTest = document.queryCommandSupported('copy');
     var elOriginalText = el.attr('data-original-title');
@@ -90,6 +102,19 @@ function setCopys(){
     }
 }
 
+function setNotif(){
+    touch = false;
+    elem = $(".custom-control-label");
+    if(lang==="es"){
+        elem.html("Notificaciones");
+    }else if(lang==="en"){
+        elem.html("Notifications");
+    }
+    if(notif){
+        $("#notif-state").prop("checked", true);
+    }
+}
+
 function create() {
     button=copy;
     popperInstance = Popper.createPopper(button, tooltip, {});
@@ -123,7 +148,6 @@ function main(){
             setButton();
         }
     });
-
     chrome.storage.local.get('clipboard', function(clip){
         if(undefined === clip.clipboard){
             content=[];
@@ -131,6 +155,15 @@ function main(){
         }else{
             content = clip.clipboard;
             setCopys();
+        }
+    });
+    chrome.storage.local.get('notif', function(a){
+        if(undefined === a.notif){
+            notif = false;
+            console.log("empty");
+        }else{
+            notif = a.notif;
+            setNotif();
         }
     });
 }
@@ -147,6 +180,7 @@ $("a").click(function(){
             lang = opc;
             setButton();
             setCopys();
+            setNotif();
         });
     }else if(opc==="mail"){
         chrome.tabs.create({url: $(this).attr("href")});
@@ -165,29 +199,33 @@ $(document).ready ( function () {
             ant.css("transition", "0,5s");
             ant.css("background-color", "");
             ant.find("p").css("color", "black");
-            ant.find("svg").css("fill", "black");
+            ant.find("#copy").css("color", "black");
             ant.find("footer").css("color", "gray");
         }
         copyToClipboard(text, el);
         $(this).parent().parent().addClass("copied");
         $(this).parent().parent().css({"transition": "1s","background-color": "green", "color": "white"});
         $(this).parent().parent().find("p").css({"transition": "1s","color": "white", "color": "white"});
-        $(this).parent().parent().find("svg").css({"transition": "0s", "fill": "white"});
+        $(this).parent().parent().find("#copy").css({"transition": "0s", "color": "white"});
         $(this).parent().parent().find("footer").css({"transition": "1s", "color": "white"});
         if(null!==popperInstance){
             hide();
         }    
         tooltip = el.parent().parent().find("#tooltip")[0];
         button = el;
-        show(el);
+        show();
     });
 });
 
-chrome.commands.onCommand.addListener(function(command){
-    if(command === "clear"){
-        chrome.storage.local.set({'clipboard': ""}, function(){
-            content = [];
+$("#notif").click(function(){
+    touch = true;
+})
+
+$("#notif-state").change(function () {
+    if(touch){
+        notif = !notif;
+        chrome.storage.local.set({'notif': notif}, function(data){
+            console.log(notif);
         });
-        $("#content").html("");
     }
-});
+}).change();
